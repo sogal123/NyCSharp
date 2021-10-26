700be7580f55783c824647ce1c1640603f7bf9b1
sogal123 <enrique.lagos.daniel@gmail.com>
2021-10-26 10:28:30 +0200

    Kategori

--- BusinessLayer/KategoriController.cs
+++ BusinessLayer/KategoriController.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DataAccessLayer.Repository;
+using Model;
 
 namespace BusinessLayer
 {
-    class KategoriController
+    public class KategoriController
     {
+        IRepository<Kategori> kategoriRepository;
+
+        public KategoriController()
+        {
+            kategoriRepository = new KategoriRepository();
+        }
+    
+    
+    public void CreateKategori(Kategori kategori)
+        {
+      
+            kategoriRepository.Create(kategori);
+            
+        }
+    
+        
     }
 }
