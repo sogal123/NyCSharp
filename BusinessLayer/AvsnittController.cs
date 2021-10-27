5b28ecb9f34eb1c11e0c515d001ac51a34363d66
sogal123 <enrique.lagos.daniel@gmail.com>
2021-10-27 15:22:27 +0200

    test

--- BusinessLayer/AvsnittController.cs
+++ BusinessLayer/AvsnittController.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using DataAccessLayer.Repository;
+using Model; 
 
 namespace BusinessLayer
 {
     public class AvsnittController
     {
+        private AvsnittRepository avsnittRepository; 
+
+        public AvsnittController()
+        {
+            avsnittRepository = new AvsnittRepository();
+        }
+
+        public List<Avsnitt> 
+
     }
 }
--- DataAccessLayer/Repository/AvsnittRepository.cs
+++ DataAccessLayer/Repository/AvsnittRepository.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Model;
+using DataAccessLayer;
 
 namespace DataAccessLayer.Repository
 {
-    class AvsnittRepository
+    public class AvsnittRepository
     {
+        public List<Avsnitt> avsnittLista;
+        public List<Podcast> podcastLista;
+        public DataManager dataManager;
+
+        public List<Avsnitt> GetAll(int i)
+        {
+            try
+            {
+                podcastLista = dataManager.DeserializePodcast();
+                Podcast podd = podcastLista[i];
+                avsnittLista = podd.Avsnitt; 
+            }
+        }
     }
 }
