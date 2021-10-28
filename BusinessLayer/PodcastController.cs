2551006b3a65273126c940c29637d2207e51c1cc
sogal123 <enrique.lagos.daniel@gmail.com>
2021-10-28 16:57:23 +0200

    FUNGERANDE CONTROLLER
    test

--- BusinessLayer/PodcastController.cs
+++ BusinessLayer/PodcastController.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Xml.Linq;
 using DataAccessLayer;
 using DataAccessLayer.Repository;
 using Model;
@@ -13,17 +9,6 @@ namespace BusinessLayer
 {
     public class PodcastController
     {
-<<<<<<< Updated upstream
-        private IRepository<Podcast> podcastRepository;
-        private List<Podcast> podcastList;
-
-        public PodcastController()
-        {
-            podcastRepository = new PodcastRepository();
-            podcastList = podcastRepository.GetAll();
-        }
-
-=======
 
         PodcastRepository podcastRepository;
         List<Podcast> podcastList;
@@ -37,24 +22,16 @@ namespace BusinessLayer
 
         }
 
->>>>>>> Stashed changes
         public List<Podcast> getAll()
         {
             return podcastRepository.GetAll();
         }
+        public void CreatePodcast(string namn, string url, string uppdateringsFrekvens, string kategori)
+        {
+            List<Avsnitt> avsnittLista = avsnittRepository.HämtaAllaAvsnitt(url);
 
-        public async Task CreatePodcastAsync(string namn, string url, string uppdateringsFrekvens, string kategori)
-        {/*, string kategori, List<Avsnitt> avsnitt*/
-            //Podcast podcast = new Podcast(namn, url, uppdateringsFrekvens, kategori); /*,  , avsnitt*/
-            //podcastRepository.Create(podcast);
-
-            await Task.Run(() =>
-            {
-                var avsnitt = hamtaAvsnittForPodcast(url);
-                Podcast podcast = new Podcast(namn, url, uppdateringsFrekvens, kategori, avsnitt);
-
-                podcastRepository.Create(podcast);
-            });
+            Podcast podcast = new Podcast(namn, url, uppdateringsFrekvens, kategori, avsnittLista);
+            podcastRepository.Create(podcast);
         }
 
         public void DeletePodcast(int i)
@@ -70,40 +47,15 @@ namespace BusinessLayer
             return poddnamn;
         }
 
-        public List<Avsnitt> hamtaAvsnittForPodcast(string url)
+        public string GetPodcastUrlAtIndex(int i)
         {
-            List<Avsnitt> episodes = new List<Avsnitt>();
-            XDocument urlDocument = new XDocument();
-            try
-            {
-                {
-                    urlDocument = XDocument.Load(url);
-                    episodes = (from x in urlDocument.Descendants("item")
-                                select new Avsnitt
-                                {
-                                    Namn = x.Element("title").Value,
-                                    AvsnittsBeskrivning = x.Element("description").Value
-                                }).ToList();
-                };
-            }
-            catch (System.Net.WebException)
-            {
-                MessageBox.Show("Network unavailable.");
-            }
-            return episodes;
+            var podcastLista = podcastRepository.GetAll();
+            Podcast valdPodcast = podcastLista[i];
+            string valdPodcastUrl = valdPodcast.Url;
+            return valdPodcastUrl;
+
         }
 
-<<<<<<< Updated upstream
-        //public string GetPodcastUrlAtIndex(int i)
-        //{
-        //    var podcastLista = podcastRepository.GetAll();
-        //    Podcast podd = podcastLista[i];
-        //    string URL = Convert.ToString(i);
-        //    string url =
-
-        //}
-    }
-=======
         public void UpdatePodcast(int valdPodcast, string namn, string url, string uppdateringsFrekvens, string kategori)
         {
             var podcastLista = podcastRepository.GetAll();
@@ -119,5 +71,4 @@ namespace BusinessLayer
         }
     }
 
->>>>>>> Stashed changes
 }
