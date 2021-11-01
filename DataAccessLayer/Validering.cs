a3c69d628b8b4843c66373bc87ce9afefa7b566c
callebuske <calle-bus@hotmail.com>
2021-11-01 14:16:12 +0100

    Create Validering.cs

---
+++ DataAccessLayer/Validering.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ServiceModel.Syndication;
+
+ 
+
+
+namespace DataAccessLayer
+{
+    class Validering
+    {
+        public static bool checkDuplicate(string url)
+        {
+            bool korrekt = true;
+            try
+            {
+                DataManager dataManager = new DataManager();
+                var podcastLista = dataManager.DeserializePodcast();
+                foreach (var podcast in podcastLista)
+                {
+                    if (podcast.Namn.Equals(url))
+                    {
+                        korrekt = false;
+                        //throw new DuplicateException();
+                    }
+                    korrekt = true;
+                }
+            }
+            catch //(DuplicateException)
+            {
+                korrekt = false;
+            }
+            return korrekt;
+        }
+
+
+    }
+}
